19a653cf8f7f6053c83f56690d23c3f40f067985
rorubyy <roruby118@gmail.com>
2023-04-14 15:38:25 +0800

    add logging info

--- flask_app/app.py
+++ flask_app/app.py
@@ -6,6 +6,7 @@ import torch
 from flask import Flask, render_template, request, make_response
 from werkzeug.exceptions import BadRequest
 import os
+import logging
 
 
 # creating flask app
@@ -16,6 +17,8 @@ dictOfModels = {}
 listOfKeys = []
 
 # inference fonction
+
+
 def get_prediction(img_bytes, model):
     img = Image.open(io.BytesIO(img_bytes))
     # inference
@@ -26,6 +29,8 @@ def get_prediction(img_bytes, model):
 # get method
 @app.route('/', methods=['GET'])
 def get():
+    app.logger.debug("I'm a DEBUG message")#TODO just for test
+
     # in the select we will have each key of the list in option
     return render_template("index.html", len=len(listOfKeys), listOfKeys=listOfKeys)
 
@@ -68,13 +73,20 @@ def extract_img(request):
     return file
 
 
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
 if __name__ == '__main__':
-    print('Starting yolov5 webservice...')
+    # starting app
+    app.logger.debug('Starting yolov5 webservice...')
     # Getting directory containing models from command args (or default 'models_train')
-    models_directory = '/home/stannyho/Documents/yolov5-flask/weights'
+    models_directory = './weights/best.pt'
     if len(sys.argv) > 1:
         models_directory = sys.argv[1]
-    print(f'Detecting for yolov5 models under {models_directory}...')
+    app.logger.debug(
+        f'Detecting for yolov5 models under {models_directory}...')
     for r, d, f in os.walk(models_directory):
         for file in f:
             if ".pt" in file:
@@ -82,12 +94,11 @@ if __name__ == '__main__':
                 # the path of each model: os.path.join(r, file)
                 model_name = os.path.splitext(file)[0]
                 model_path = os.path.join(r, file)
-                print(f'Loading model {model_path} with path {model_path}...')
+                app.logger.debug(
+                    f'Loading model {model_path} with path {model_path}...')
                 dictOfModels[model_name] = torch.hub.load(
                     'ultralytics/yolov5', 'custom', path=model_path)
                 # you would obtain: dictOfModels = {"model1" : model1 , etc}
         for key in dictOfModels:
             listOfKeys.append(key)  # put all the keys in the listOfKeys
-
-    # starting app
     app.run(debug=True, host='0.0.0.0')
